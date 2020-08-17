73b11c183889415a05c12e5f17c65d6f0d863d2f
Joon S. Lee <dolomite00@gmail.com>
2020-08-17 04:24:04 -0400

    method overloading examples

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
-print("The owner of the original repository made this change!")
-print("GIT is FUN")
+def addAll():
+    return 0
+
+
+def addAll(num):
+    return num
+
+
+def addAll(num1, num2):
+    return num1 + num2
+
+
+def addAll(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+# print(addAll())
+print(addAll(1))
+print(addAll(2, 3))
+print(addAll(1, 2, 3))
+
--- test2.py
+++ test2.py
@@ -1,1 +1,29 @@
-print("file2")
+# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+
+from multipledispatch import dispatch
+
+# passing one parameter
+@dispatch(int, int)
+def product(first, second):
+    result = first * second
+    print(result)
+
+
+# passing two parameters
+@dispatch(int, int, int)
+def product(first, second, third):
+    result = first * second * third
+    print(result)
+
+
+# you can also pass data type of any value as per requirement
+@dispatch(float, float, float)
+def product(first, second, third):
+    result = first * second * third
+    print(result)
+
+
+# calling product method with 2 arguments
+product(2, 3, 2)
+product(2.2, 3.4, 2.3)
+
